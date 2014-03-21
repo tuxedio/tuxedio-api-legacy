ce09436424b3f3793c94b9c86f00f24b7dd56d65
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-03-20 18:56:27 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TuxedoProto::Application.config.secret_key_base = '254d2be9e22b6a28d9a37ed62ab5679155dc28d6474ebbcb356b672d9375d4496f67115a129ce4a372aa6fb4f5cbdb9796fa5241dd31ba546ddb0ba31474c3cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TuxedoProto::Application.config.session_store :cookie_store, key: '_tuxedo_proto_session'
