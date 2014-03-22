047e96d6ea67bdfdda91aba49341f847422e00dc
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-03-22 12:31:58 -0600

    Move custom_validations to its own file

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ActiveRecord::Base
   validates :location,     presence: true, length: { maximum: 30 }
   validates :email,        presence: true, email: true,
                            uniqueness: { case_sensitive: false }
+  validates :password,     length: { minimum: 6 }
+  has_secure_password
+
   validates :vendor,       :inclusion => { :in => [true, false] }
   validates :customer,     :inclusion => { :in => [true, false] }
   validates :blogger,      :inclusion => { :in => [true, false] }
