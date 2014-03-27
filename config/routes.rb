e14e3c353a0399e014a1e61b94de10f0d78673e2
thejinxters <jinx122985@yahoo.com>
2014-03-27 14:29:48 -0600

    create static pages and begin work on views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TuxedoProto::Application.routes.draw do
+  get "static_pages/home"
   get "users/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
