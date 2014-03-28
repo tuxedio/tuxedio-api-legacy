46be9e802ee211bba4a314a083bbc0e49c23c9da
thejinxters <jinx122985@yahoo.com>
2014-03-28 16:30:37 -0600

    create form for signup and add cucumber testing environment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,31 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
   def new
     @user = User.new
   end
   def create
     @user = User.new(user_params)
+    if params[:vendor] == true
+      @user[:customer] = false
+    else
+      @user[:customer] = true
+    end
+    @user[:blogger] = false
     if @user.save
       flash[:success] = "Welcome to the Sample App"
       redirect_to @user
     else
+      flash[:error] = "Something Went Wrong"
       render 'new'
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :location, :email, :password, :password_confirmation, :vendor)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ TuxedoProto::Application.routes.draw do
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   get "static_pages/home"
-  get "users/new"
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
