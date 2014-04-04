54f53d92eab08f7a06ae2aebc4ec478a9032d5a6
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-04-04 17:39:21 -0600

    Linting

--- config/deploy/current_branch.rb
+++ config/deploy/current_branch.rb
@@ -2,9 +2,6 @@ def current_git_branch
   branch = `git symbolic-ref HEAD 2> /dev/null`.strip.gsub(/^refs\/heads\//, '')
 end
 
-# Set the deploy branch to the current branch
 set :stage, :production
 set :branch, current_git_branch
-
-# Replace 127.0.0.1 with your server's IP address!
 server '107.170.226.89', user: 'deploy', roles: %w{web app}
--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,5 +1,3 @@
 set :stage,  :production
 set :branch, 'development'
-
-# Replace 127.0.0.1 with your server's IP address!
 server '107.170.226.89', user: 'deploy', roles: %w{web app}
