9a6e740a61578c72428f7e0f1884f136b14a09c5
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-04-04 17:32:57 -0600

    Add current_branch deploy feature

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,21 +2,7 @@ set :application, 'tuxedo'
 set :repo_url, 'git@github.com:tuxedio/tuxedo-proto.git'
 
 set :deploy_to, '/home/deploy/tuxedo'
-
-# Bonus! Colors are pretty!
-def red(str)
-  "\e[31m#{str}\e[0m"
-end
-
-# Figure out the name of the current local branch
-def current_git_branch
-  branch = `git symbolic-ref HEAD 2> /dev/null`.strip.gsub(/^refs\/heads\//, '')
-  puts "Deploying branch #{red branch}"
-  branch
-end
-
-# Set the deploy branch to the current branch
-set :branch, current_git_branch
+set :deploy_via, :remote_cache
 
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
-set :stage, :production
+set :stage,  :production
+set :branch, 'development'
 
 # Replace 127.0.0.1 with your server's IP address!
 server '107.170.226.89', user: 'deploy', roles: %w{web app}
