7ffc38a4488c9debfa40048f892c71fcbdc201f0
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-04-05 13:12:29 -0600

    Fix typo

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :stage,  :production
-set :branch, ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
+set :branch, ENV["REVISION"] || ENV["BRANCH_NAME"] || "deploy"
 server '107.170.226.89', user: 'deploy', roles: %w{web app}
