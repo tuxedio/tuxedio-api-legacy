aed61cc392f188099f0dca1d7adbf7fa0db00e0f
thejinxters <jinx122985@yahoo.com>
2014-04-05 14:27:53 -0600

    Add deploy from branch by using 'cap production deploy branch=branch_name'

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,15 +8,8 @@ def red(str)
   "\e[31m#{str}\e[0m"
 end
 
-# Figure out the name of the current local branch
-def current_git_branch
-  branch = `git symbolic-ref HEAD 2> /dev/null`.strip.gsub(/^refs\/heads\//, '')
-  puts "Deploying branch #{red branch}"
-  branch
-end
-
 # Set the deploy branch to the current branch
-set :branch, current_git_branch
+set :branch, ENV['branch'] || 'development'
 
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
