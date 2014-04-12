2873318dbc6c53effc8e7b552fa0fdeaedc826ce
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-04-12 11:51:31 -0600

    Fix migrations

--- db/migrate/20140412152355_add_devise_to_users.rb
+++ db/migrate/20140412152355_add_devise_to_users.rb
@@ -2,9 +2,7 @@ class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
     change_table(:users) do |t|
       ## Database authenticatable
-      t.string :name,               :null => false, :default => ""
       t.string :email,              :null => false, :default => ""
-      t.string :location,           :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
 
       ## Recoverable
