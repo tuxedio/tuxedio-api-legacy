4601e2aa19d92fd10f65e2a85232c4b1f1654616
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-04-12 11:03:35 -0600

    Make sign_up with location work

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :name, :email, :location, :password, :password_confirmation
+  #attr_accessible :name, :email, :location, :password, :password_confirmation
 
   self.inheritance_column = :user_role
   scope :customer, -> { where(user_role: 'Customer')}
