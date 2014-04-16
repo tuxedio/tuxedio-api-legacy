3f770c57865b84c994c01e4a13b0447140418187
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-04-16 11:45:52 -0600

    Add validitions for activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,10 +1,13 @@
 class Activity < ActiveRecord::Base
   attr_accessible   :name, :location, :price, :description, :vendor_id
+  PRICE_REGEX = /\A^\d+??(?:\.\d{0,2})?$\z/ 
 
   validates  :name,         presence: true, length: { maximum: 30 }
   validates  :location,     presence: true, length: { maximum: 30 }
   validates  :price,        presence: true,
-                            numericality: {:greater_than => 0}
-  validates  :vendor_id,    presence: true
+                            numericality:   { greater_than_or_equal_to: 0 },
+                            :format => { :with => PRICE_REGEX }
+  validates  :vendor_id,    presence: true, uniqueness: true
+  
   belongs_to :vendor
 end
