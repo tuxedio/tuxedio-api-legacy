f41f0350a17f244397c5aed41862b88395d05500
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-04-16 17:30:33 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,27 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Vendor.create(name: "Larkburger", email: "Vendor1@example.com", location: "Boulder",
-             password: "mypassword1", password_confirmation: "mypassword1")
+vend1 = Vendor.create!(name: "Larkburger", email: "Vendor1@example.com", location: "Boulder",
+             password: "mypassword1", password_confirmation: "mypassword1", zip_code: 80301)
+vend1.confirm!
 
-Vendor.create(name: "Sushi Tora", email: "Vendor2@example.com", location: "Boulder",
-             password: "mypassword1", password_confirmation: "mypassword1")
+vend2 = Vendor.create!(name: "Larkburger", email: "Vendor2@example.com", location: "Boulder",
+             password: "mypassword1", password_confirmation: "mypassword1", zip_code: 80301)
+vend2.confirm!
 
-Vendor.create(name: "Illegal Pete's", email: "Vendor3@example.com", location: "Boulder",
+vend3 = Vendor.create!(name: "Larkburger", email: "Vendor3@example.com", location: "Boulder",
+             password: "mypassword1", password_confirmation: "mypassword1", zip_code: 80301)
+vend3.confirm!
+
+
+cust1 = Customer.create!(name: "John", email: "customer1@example.com", location: "Boulder",
              password: "mypassword1", password_confirmation: "mypassword1")
+cust1.confirm!
+
+cust2 = Customer.create!(name: "Adam", email: "customer2@example.com", location: "Boulder",
+             password: "mypassword1", password_confirmation: "mypassword1")
+cust2.confirm!
+
+cust3 = Customer.create!(name: "Eve", email: "customer3@example.com", location: "Boulder",
+             password: "mypassword1", password_confirmation: "mypassword1")
+cust3.confirm!
