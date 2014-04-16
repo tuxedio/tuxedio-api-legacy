0dff55e496227faf0c0b3005e420184a4f745980
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-04-16 12:18:43 -0600

    Add RSpec tests for activity validation

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,13 +1,16 @@
 class Activity < ActiveRecord::Base
-  attr_accessible   :name, :location, :price, :description, :vendor_id
   PRICE_REGEX = /\A^\d+??(?:\.\d{0,2})?$\z/ 
 
-  validates  :name,         presence: true, length: { maximum: 30 }
-  validates  :location,     presence: true, length: { maximum: 30 }
+  attr_accessible   :name, :location, :price, :description, :vendor_id
+
+  validates  :name,         presence: true, length: { maximum: 30  }
+  validates  :location,     presence: true, length: { maximum: 30  }
+  validates  :description,  presence: true, length: { maximum: 160 }
   validates  :price,        presence: true,
                             numericality:   { greater_than_or_equal_to: 0 },
-                            :format => { :with => PRICE_REGEX }
+                            format: { :with => PRICE_REGEX }
   validates  :vendor_id,    presence: true, uniqueness: true
+  validates_presence_of :vendor
   
   belongs_to :vendor
 end
