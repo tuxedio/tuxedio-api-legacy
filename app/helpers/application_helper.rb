aa24fd9c59042937545779be54ebd9c2da38d6be
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-04-18 14:03:38 -0600

    Minor view tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis
-  def full_title(page_title)
+  def full_title( page_title )
     base_title = "Tuxedo"
     if page_title.empty?
       base_title
@@ -9,4 +9,13 @@ module ApplicationHelper
     end
   end
 
+  def flash_class( level )
+    case level
+      when :notice then "alert alert-info"
+      when :success then "alert alert-success"
+      when :danger then "alert alert-danger"
+      when :alert then "alert alert-warning"
+      when :warning then "alert alert-warning"
+    end
+  end
 end
