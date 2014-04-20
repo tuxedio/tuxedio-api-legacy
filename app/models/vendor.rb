b76df65b9b170496b0a835dce3c8b425479d192b
IndieSquidge <a.gilman.wood@gmail.com>
2014-04-20 13:13:43 -0600

    Add profile picture to vendor

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -9,7 +9,7 @@ class Vendor < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   #ACCESS ATTRIBUTES OF THE MODEL
-  attr_accessible :name, :email, :location, :zip_code, :password, :password_confirmation, :vendor_id, :description
+  attr_accessible :name, :email, :location, :zip_code, :password, :password_confirmation, :vendor_id, :description, :picture
 
   #VALIDATION
   validates :name,         presence: true, length: { maximum: 50 }
@@ -25,5 +25,8 @@ class Vendor < ActiveRecord::Base
   def self.allowed_locations
     ['Boulder', 'Longmont', 'Broomfield']
   end
+
+  has_attached_file :picture, :styles => { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 
 end
