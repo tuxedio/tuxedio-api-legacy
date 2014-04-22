fb5012fe41db9c9be545c43a5970984eeeff1818
IndieSquidge <a.gilman.wood@gmail.com>
2014-04-22 09:10:31 -0600

    Scale photos to fit into square

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -14,6 +14,6 @@ class Activity < ActiveRecord::Base
 
   belongs_to :vendor
 
-  has_attached_file :picture, :styles => { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :picture, :styles => { medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 end
