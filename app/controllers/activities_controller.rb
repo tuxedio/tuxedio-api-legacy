031ecdf98081a5a74da54ad4fdcd61d1b71163f3
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-04-22 16:19:10 -0600

    Init explore routes and controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -35,6 +35,9 @@ class ActivitiesController < ApplicationController
     end
   end
 
+  def explore
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,22 @@ TuxedoProto::Application.routes.draw do
 
   #ROUTES
   root to: 'static_pages#home'
-  match  '/registration_choice', to: 'static_pages#registration_choice', via: 'get'
-  match '/customer_profile', to: 'customers#show', via: 'get'
-  match '/vendor_profile', to: 'vendors#show', via: 'get'
+  match '/registration_choice', to: 'static_pages#registration_choice', via: 'get'
+  match '/customer_profile',    to: 'customers#show',                   via: 'get'
+  match '/vendor_profile',      to: 'vendors#show',                     via: 'get'
+  match '/explore',             to: 'activities#explore',               via: 'get'
 
   # Routes for Yelp information
   resource :vendors do
     collection do
       get 'confirm_details'
       get 'update_details'
+    end
+  end
+
+  resource :activities do
+    collection do
+      get 'explore'
     end
   end
 
