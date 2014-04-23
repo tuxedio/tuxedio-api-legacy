57a249515301a86f05d9fb0768110dee71406810
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-04-22 23:36:29 -0600

    Add validations to belongs_to assocations

--- app/models/itinerary_item.rb
+++ app/models/itinerary_item.rb
@@ -1,10 +1,14 @@
 class ItineraryItem < ActiveRecord::Base
+
   #ACCESSORS
   attr_accessible :activity_id, :trip_id
 
   #VALIDATION
-  validates :trip_id,  presence: true
-  validates :activity_id,   presence: true
+  validates :trip_id,      presence: true
+  validates :activity_id,  presence: true
+
+  validates_presence_of :trip
+  validates_presence_of :activity
 
   #ASSOCIATIONS
   belongs_to :trip
