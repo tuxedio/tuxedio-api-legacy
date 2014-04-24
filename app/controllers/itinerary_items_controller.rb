37f38e100a214f077e58a6d85f6f2969205c3524
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-04-23 20:40:14 -0600

    Add activity to trip functioning

--- app/controllers/itinerary_items_controller.rb
+++ app/controllers/itinerary_items_controller.rb
@@ -2,27 +2,33 @@ class ItineraryItemsController < ApplicationController
 
   def new
     @activity = Activity.find(params[:activity_id])
-    @itinerary_item = ItineraryItem.new(activity_id: @activity.id)
+    @itinerary_item = ItineraryItem.new
+
     @trips = current_customer.trips
+
+
   end
 
   def create
-    @activity = Activity.find(params[:activity_id])
-    @trips = current_customer.trips
-    @itinerary_item = ItineraryItem.new(itinerary_items_params)
+    @trip = current_customer.trips.find_by_id(itinerary_items_params['trip_id'])
+    @itinerary_item = @trip.itinerary_items.create(itinerary_items_params)
+
     if @itinerary_item.save
-      redirect_to root_path
+      redirect_to explore_path
     else
       render 'new'
     end
+
   end
 
-  def itinerary_items_params
-    params.require(:itinerary_item).permit(
-      :trip_id,
-      :activity_id,
-      :start_time,
-      :end_time
-    )
-  end
+  private
+
+    def itinerary_items_params
+      params.require(:itinerary_item).permit(
+        :trip_id,
+        :activity_id,
+        :start_time,
+        :end_time
+      )
+    end
 end
