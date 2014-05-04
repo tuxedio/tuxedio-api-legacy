904e77ec5146fc5de65c708c81bd3c8835856b35
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-05-04 11:31:58 -0600

    Clean up customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,37 +1,62 @@
 class Customer < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
 
-  before_save{ email.downcase! }
-
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
-         :validatable, :confirmable
+  #--------------------------------------------------------
+  # Attributes
 
   attr_accessible :name, :email, :location, :password,
                   :password_confirmation, :bio,
                   :top_choices, :picture_file_size, :picture,
-                  :current_trip
+                  :current_trip, :top_choices
+
+  serialize :top_choices, Array
+
+
+  #--------------------------------------------------------
+  # Associations
+
+  has_many :trips
+
+
+  #--------------------------------------------------------
+  # Validations
 
   validates :name,         presence: true, length: { maximum: 50 }
   validates :location,     presence: true, length: { maximum: 30 }
   validates :email,        presence: true, email: true, uniqueness: {case_sensitive: false }
   validates :top_choices,  choice: true, on: :update
 
-  serialize :top_choices, Array
-
+  # Eventually refactor this and move this logic to production.rb
   if Rails.env == 'production'
     has_attached_file :picture, :styles => { medium: "300x300#", thumb: "100x100#" }, default_url: 'missing_avatar.jpg'
   else
     has_attached_file :picture, :styles => { medium: "300x300#", thumb: "100x100#" }, default_url: ActionController::Base.helpers.asset_path('missing_avatar.jpg')
   end
 
-  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
-  validates :picture_file_size, numericality: { less_than_or_equal_to: 10000000 }, allow_nil: true
+  validates_attachment_content_type :picture,
+    :content_type => /^image\/(png|gif|jpeg|jpg)/,
+    :message => 'Only .png .gif .jpeg .jpg images, please.'
 
-  #ASSOCIATIONS
-  has_many :trips
+  validates_attachment_size :picture,
+    :less_than => 10.megabytes,
+    :message => "Picture too large."
+
+
+  #--------------------------------------------------------
+  # Callbacks
+
+  before_save{ email.downcase! }
+
+
+  #--------------------------------------------------------
+  # Other Macros
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable,
+         :validatable, :confirmable
+
+
+  #--------------------------------------------------------
+  # Instance Methods
 
   def current_trip(trip_id)
     if self.trips.count > 0
@@ -42,4 +67,6 @@ class Customer < ActiveRecord::Base
       end
     end
   end
+
+
 end
