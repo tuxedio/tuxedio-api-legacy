c83b2080b0d9d96f6a0aee02665cec5fa27949b6
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-06-05 16:28:30 -0600

    Refactor RSpec tests

--- spec/models/trips_spec.rb
+++ spec/models/trips_spec.rb
@@ -1,75 +1,95 @@
 require 'spec_helper'
 
-describe "Trips".upcase.colorize(:light_blue) do
+describe Trip do
 
-  before do
-    @trip = FactoryGirl.create(:trip)
+  subject(:trip) { FactoryGirl.build(:trip) }
+
+  describe "instantiation of trip" do
+
+    it "should be a trip" do
+      expect(trip).to be_a(Trip)
+      expect(trip).to be_valid
+    end
   end
 
-  subject{ @trip }
-  it { should respond_to(:trip_name) }
-  it { should respond_to(:location) }
-  it { should respond_to(:start_date) }
-  it { should respond_to(:number_of_days) }
-  it { should respond_to(:customer) }
-
-  it { should be_valid }
-
-  describe "Check parameters for blankness".upcase.colorize(:light_blue) do
-
-    describe "when customer is not present" do
-      before { @trip.customer= nil }
-      it { should_not be_valid }
+  describe "trip_name" do
+    it "should respond to trip_name" do
+      expect(trip).to respond_to(:trip_name)
     end
 
-    describe "when trip name is not present" do
-      before { @trip.trip_name = nil }
-      it { should_not be_valid }
-    end
-
-    describe "when location is not present" do
-      before { @trip.location = nil }
-      it { should_not be_valid }
-    end
-
-    describe "when number of days is not present" do
-      before { @trip.number_of_days = nil }
-      it { should_not be_valid }
-    end
-
-    describe "Unacceptable input".upcase.colorize(:light_blue) do
-      describe "when number of days is zero" do
-        before { @trip.number_of_days = 0 }
-        it { should_not be_valid }
-      end
-
-      describe "when location is not in valid location" do
-        before { @trip.location = "Albania" }
-        it { should_not be_valid }
-      end
-
-      describe "when start date is before the current date" do
-        before { @trip.location = Date.yesterday }
-        it { should_not be_valid }
+    describe "when trip_name is not present" do
+      before { trip.trip_name = nil }
+      it "should not be valid" do
+        expect(trip).to_not be_valid
       end
     end
-
-    describe "Acceptable input".upcase.colorize(:light_blue) do
-      describe "when trip name is present" do
-        before { @trip.trip_name = "Test " }
-        it { should be_valid }
+    describe "when trip name is too long" do
+      before { trip.trip_name = 'a' * 21 }
+      it "should not be valid" do
+        expect(trip).to_not be_valid
       end
+    end
+  end
 
-      describe "when location is present and valid" do
-        before { @trip.location = "Boulder" }
-        it { should be_valid }
+  describe "location" do
+    it "should respond to location" do
+      expect(trip).to respond_to(:location)
+    end
+    describe "when it is in an unacceptable location" do
+      before { trip.location = "Some unacceptable location" }
+      it "should not be valid" do
+        expect(trip).to_not be_valid
       end
+    end
+  end
 
-      describe "when number of days is present" do
-        before { @trip.number_of_days = 10 }
-        it { should be_valid }
+  describe "customer" do
+    it "should respond to customer" do
+      expect(trip).to respond_to(:customer)
+    end
+    describe "when customer is destroyed" do
+      before { trip.customer.destroy }
+      it "should also destroy associated trips" do
+        expect(Trip.count).to equal(0)
       end
+    end
+  end
 
+  describe "start_date" do
+    it "should respond to start_date" do
+      expect(trip).to respond_to(:start_date)
+    end
+
+    describe "when start_date is not present" do
+      before { trip.start_date = nil }
+      it "should not be valid" do
+        expect(trip).to_not be_valid
+      end
+    end
+    describe "when start_date is before current time" do
+      before { trip.start_date = Time.now.yesterday }
+      it "should not be valid" do
+        expect(trip).to_not be_valid
+      end
+    end
+  end
+
+  describe "number_of_days" do
+    it "should respond to number_of_days" do
+      expect(trip).to respond_to(:number_of_days)
+    end
+
+    describe "when number_of_days is not present" do
+      before { trip.number_of_days = nil }
+      it "should not be valid" do
+        expect(trip).to_not be_valid
+      end
+    end
+    describe "when number_of_days is too short" do
+      before { trip.number_of_days = -5 }
+      it "should not be valid" do
+        expect(trip).to_not be_valid
+      end
     end
   end
 end
