f826d427d650efad6d7c1714cb1ba7844914ac9e
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-06-05 16:03:38 -0600

    Reorganize model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,21 +1,27 @@
 class Trip < ActiveRecord::Base
 
-  #ACCESSORS
+  #--------------------------------------------------------
+  # Attributes
+
   attr_accessible :trip_name, :customer, :location, :start_date, :number_of_days, :id, :activity
 
-  #VALIDATION
+  #--------------------------------------------------------
+  # Associations
+
+  belongs_to :customer
+  has_many :itinerary_items, dependent: :destroy
+  has_many :activities, through: :itinerary_items
+
+  #--------------------------------------------------------
+  # Validations
+
   validates :trip_name,       presence: true, length: { maximum: 20 }
   validates :customer,        presence: true
   validates :start_date,      presence: true
   validates :number_of_days,  presence: true, numericality: { greater_than: 0 }
   validates :customer,        presence: true
+
   validates_date :start_date, on_or_after: Time.now
-
   validates_with LocationValidator
-
-  #ASSOCIATIONS
-  belongs_to :customer
-  has_many :itinerary_items, dependent: :destroy
-  has_many :activities, through: :itinerary_items
 
 end
