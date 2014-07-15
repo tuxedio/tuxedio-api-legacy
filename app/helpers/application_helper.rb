9708d3dec4ffa7489e016646fa106ad49d238a41
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-07-14 22:57:46 -0600

    Upgrade to Rails 4.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,11 +11,11 @@ module ApplicationHelper
 
   def flash_class( level )
     case level
-      when :notice then "alert alert-info"
-      when :success then "alert alert-success"
-      when :danger then "alert alert-danger"
-      when :alert then "alert alert-warning"
-      when :warning then "alert alert-warning"
+      when 'notice'  then 'alert alert-info'
+      when 'success' then 'alert alert-success'
+      when 'danger'  then 'alert alert-danger'
+      when 'alert'   then 'alert alert-warning'
+      when 'warning' then 'alert alert-warning'
     end
   end
 end
